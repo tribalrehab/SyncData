17e125435ff1b482d1b0e68ffec366ae7f89677f
tribalrehab <tribalrehab@gmail.com>
2025-01-06 15:42:23 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        GroupBox1 = New GroupBox()
+        ComboBox1 = New ComboBox()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        GroupBox2 = New GroupBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        NotifyIcon1 = New NotifyIcon(components)
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        SettingsToolStripMenuItem = New ToolStripMenuItem()
+        ManualSyncToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        Timer1 = New Timer(components)
+        ProgressBar1 = New ProgressBar()
+        Button2 = New Button()
+        Label3 = New Label()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        ContextMenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(ComboBox1)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(886, 203)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Configuration"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(6, 136)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(298, 33)
+        ComboBox1.TabIndex = 4
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(6, 108)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(198, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "Automatic Sync Interval"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(6, 55)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(874, 31)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(6, 27)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(145, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Backup Location:"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(TextBox2)
+        GroupBox2.Location = New Point(12, 221)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(886, 278)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Folder Paths"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(6, 30)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(874, 240)
+        TextBox2.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(786, 505)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 2
+        Button1.Text = "Done"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' NotifyIcon1
+        ' 
+        NotifyIcon1.ContextMenuStrip = ContextMenuStrip1
+        NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), Icon)
+        NotifyIcon1.Text = "SyncData"
+        NotifyIcon1.Visible = True
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.ImageScalingSize = New Size(24, 24)
+        ContextMenuStrip1.Items.AddRange(New ToolStripItem() {SettingsToolStripMenuItem, ManualSyncToolStripMenuItem, ExitToolStripMenuItem})
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(184, 100)
+        ' 
+        ' SettingsToolStripMenuItem
+        ' 
+        SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        SettingsToolStripMenuItem.Size = New Size(183, 32)
+        SettingsToolStripMenuItem.Text = "Settings"
+        ' 
+        ' ManualSyncToolStripMenuItem
+        ' 
+        ManualSyncToolStripMenuItem.Name = "ManualSyncToolStripMenuItem"
+        ManualSyncToolStripMenuItem.Size = New Size(183, 32)
+        ManualSyncToolStripMenuItem.Text = "Manual Sync"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(183, 32)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(389, 505)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(391, 34)
+        ProgressBar1.TabIndex = 3
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(18, 505)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(169, 34)
+        Button2.TabIndex = 4
+        Button2.Text = "Manual Sync"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.Location = New Point(210, 505)
+        Label3.Name = "Label3"
+        Label3.RightToLeft = RightToLeft.Yes
+        Label3.Size = New Size(173, 30)
+        Label3.TabIndex = 5
+        Label3.Text = "Progress"
+        Label3.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(910, 549)
+        Controls.Add(Label3)
+        Controls.Add(Button2)
+        Controls.Add(ProgressBar1)
+        Controls.Add(Button1)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "SyncData"
+        WindowState = FormWindowState.Minimized
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ContextMenuStrip1.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ManualSyncToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,292 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Security.Cryptography
+Imports System.IO
+
+
+
+Module INIHandler
+    ' Declare the functions from kernel32.dll
+    <DllImport("kernel32.dll", CharSet:=CharSet.Auto)>
+    Private Function WritePrivateProfileString(lpAppName As String, lpKeyName As String, lpString As String, lpFileName As String) As Boolean
+    End Function
+
+    <DllImport("kernel32.dll", CharSet:=CharSet.Auto)>
+    Private Function GetPrivateProfileString(lpAppName As String, lpKeyName As String, lpDefault As String, lpReturnedString As StringBuilder, nSize As Integer, lpFileName As String) As Integer
+    End Function
+
+    ' Write to INI File
+    Public Sub WriteToINI(section As String, key As String, value As String, filePath As String)
+        WritePrivateProfileString(section, key, value, filePath)
+    End Sub
+
+    ' Read from INI File
+    Public Function ReadFromINI(section As String, key As String, filePath As String, Optional defaultValue As String = "") As String
+        Dim returnValue As New StringBuilder(255)
+        GetPrivateProfileString(section, key, defaultValue, returnValue, returnValue.Capacity, filePath)
+        Return returnValue.ToString()
+    End Function
+
+    ' Save multiline text to INI
+    Public Sub WriteMultilineToINI(section As String, key As String, value As String, filePath As String)
+        ' Replace line breaks with a unique placeholder
+        Dim singleLineValue As String = value.Replace(Environment.NewLine, "|")
+        WritePrivateProfileString(section, key, singleLineValue, filePath)
+    End Sub
+
+    ' Read multiline text from INI
+    Public Function ReadMultilineFromINI(section As String, key As String, filePath As String, Optional defaultValue As String = "") As String
+        Dim singleLineValue As New StringBuilder(255)
+        GetPrivateProfileString(section, key, defaultValue, singleLineValue, singleLineValue.Capacity, filePath)
+        ' Replace placeholder with line breaks
+        Return singleLineValue.ToString().Replace("|", Environment.NewLine)
+    End Function
+
+End Module
+Public Class Form1
+    Private iniFilePath As String = IO.Path.Combine(Application.StartupPath, "settings.ini")
+    Public Function PathNumber(folderPath As String) As Integer
+        ' Ensure the folder path is consistent (e.g., lowercase, trimmed)
+        Dim normalizedPath As String = folderPath.Trim().ToLower()
+
+        ' Compute a hash value using MD5 (or SHA256 for more complexity)
+        Using md5 As MD5 = MD5.Create()
+            Dim hashBytes As Byte() = md5.ComputeHash(Encoding.UTF8.GetBytes(normalizedPath))
+
+            ' Convert the first 4 bytes of the hash to an integer
+            ' This creates a unique, deterministic number for the path
+            Dim uniqueNumber As Integer = BitConverter.ToInt32(hashBytes, 0)
+
+            Return Math.Abs(uniqueNumber).ToString()
+            ' Ensure it's non-negative
+        End Using
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Hide()
+        'Populate UI Elements
+        populateUIElements()
+
+        'Load UI elements from configuration file
+        loadUIElements()
+
+        'NotifyIcon Configuration
+
+        NotifyIcon1.ContextMenuStrip = ContextMenuStrip1
+        NotifyIcon1.Visible = True
+
+
+    End Sub
+
+    Sub populateUIElements()
+        Label3.Text = ""
+        ComboBox1.Items.Add("5 Minutes")
+        ComboBox1.Items.Add("10 Minutes")
+        ComboBox1.Items.Add("15 Minutes")
+        ComboBox1.Items.Add("30 Minutes")
+        ComboBox1.Items.Add("Every Hour")
+        ComboBox1.Items.Add("Every 4 Hours")
+        ComboBox1.Items.Add("Every 8 Hours")
+        ComboBox1.Items.Add("Every 12 Hours")
+    End Sub
+
+    Sub loadUIElements()
+        TextBox1.Text = ReadMultilineFromINI("BackupLocation", "Path", iniFilePath, "")
+        UpdateIniFilePath()
+
+        TextBox2.Text = ReadMultilineFromINI("FolderPaths", "Paths", iniFilePath, "")
+        Dim intervalSetting As String = ReadFromINI("TimerSettings", "Interval", iniFilePath, "Every Hour")
+        ComboBox1.SelectedItem = intervalSetting
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ' Configure the timer for each selected interval
+        Select Case ComboBox1.SelectedItem.ToString()
+            Case "5 Minutes"
+                Timer1.Interval = 5 * 60 * 1000 ' 5 minutes in milliseconds
+            Case "10 Minutes"
+                Timer1.Interval = 10 * 60 * 1000 ' 10 minutes in milliseconds
+            Case "15 Minutes"
+                Timer1.Interval = 15 * 60 * 1000 ' 15 minutes in milliseconds
+            Case "30 Minutes"
+                Timer1.Interval = 30 * 60 * 1000 ' 30 minutes in milliseconds
+            Case "Every Hour"
+                Timer1.Interval = 60 * 60 * 1000 ' 1 hour in milliseconds
+            Case "Every 4 Hours"
+                Timer1.Interval = 4 * 60 * 60 * 1000 ' 4 hours in milliseconds
+            Case "Every 8 Hours"
+                Timer1.Interval = 8 * 60 * 60 * 1000 ' 8 hours in milliseconds
+            Case "Every 12 Hours"
+                Timer1.Interval = 12 * 60 * 60 * 1000 ' 12 hours in milliseconds
+        End Select
+
+        ' Save settings to INI file
+        SaveSettings()
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub SaveSettings()
+        If ComboBox1.SelectedItem = "" Then
+            ComboBox1.SelectedItem = "Every Hour"
+        End If
+        WriteToINI("TimerSettings", "Interval", ComboBox1.SelectedItem.ToString(), iniFilePath)
+        WriteToINI("BackupLocation", "Path", TextBox1.Text, iniFilePath)
+        WriteMultilineToINI("FolderPaths", "Paths", TextBox2.Text, iniFilePath)
+    End Sub
+
+
+
+    Private Sub TextBox2_Leave(sender As Object, e As EventArgs) Handles TextBox2.Leave
+        ' Save configuration
+        SaveSettings()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_Leave(sender As Object, e As EventArgs) Handles TextBox1.Leave
+        ' Save configuration
+        SaveSettings()
+
+        'Reload settings from filepath
+
+    End Sub
+    Private Sub UpdateIniFilePath()
+        Dim newPath As String = TextBox1.Text
+
+        ' Validate if the path is valid and exists
+        If IO.Directory.Exists(newPath) Then
+            ' Combine the directory with a filename for the INI file
+            iniFilePath = IO.Path.Combine(newPath, "settings.ini")
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SaveSettings()
+        Me.Hide()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        e.Cancel = True ' Cancel the close event
+        Me.Hide()
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub SettingsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SettingsToolStripMenuItem.Click
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+
+    Sub SyncPaths()
+        Label3.Text = "Synchronizing ... "
+        Dim linesArray As String() = TextBox2.Lines
+        For Each line As String In linesArray
+            Dim source = line.Replace("%USERNAME%", Environment.UserName)
+            Dim destination = TextBox1.Text
+            destination = Path.Combine(destination, "Data", PathNumber(line))
+
+            SynchronizeFolders(source, destination)
+            SynchronizeFolders(destination, source)
+
+        Next
+        Label3.Text = "Sync Completed"
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        SyncPaths()
+    End Sub
+
+    Private Sub ManualSyncToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ManualSyncToolStripMenuItem.Click
+        SyncPaths()
+    End Sub
+    Function GenerateUniqueString(input As String) As String
+        ' Ensure the input is not null or empty.
+        If String.IsNullOrEmpty(input) Then
+            Throw New ArgumentException("Input string cannot be null or empty.")
+        End If
+
+        ' Use SHA256 to compute the hash of the input string.
+        Using sha256 As SHA256 = SHA256.Create()
+            ' Convert the input string to a byte array.
+            Dim inputBytes As Byte() = Encoding.UTF8.GetBytes(input)
+
+            ' Compute the hash.
+            Dim hashBytes As Byte() = sha256.ComputeHash(inputBytes)
+
+            ' Convert the hash bytes to a Base64 string for readability.
+            Return Convert.ToBase64String(hashBytes)
+        End Using
+    End Function
+    Public Sub SynchronizeFolders(sourceFolder As String, targetFolder As String)
+        ' Ensure the target directory exists.
+        If Not Directory.Exists(targetFolder) Then
+            Directory.CreateDirectory(targetFolder)
+        End If
+
+        ' Get all files from the source folder.
+        Dim sourceFiles = Directory.GetFiles(sourceFolder, "*", SearchOption.AllDirectories)
+
+        ' Configure the progress bar.
+        ProgressBar1.Maximum = sourceFiles.Length
+        ProgressBar1.Value = 0
+
+        For Each sourceFile In sourceFiles
+            ' Determine the relative path of the file.
+            Dim relativePath = Path.GetRelativePath(sourceFolder, sourceFile)
+
+            ' Map the relative path to the target folder.
+            Dim targetFile = Path.Combine(targetFolder, relativePath)
+
+            ' Ensure the target subdirectory exists.
+            Dim targetDirectory = Path.GetDirectoryName(targetFile)
+            If Not Directory.Exists(targetDirectory) Then
+                Directory.CreateDirectory(targetDirectory)
+            End If
+
+            ' Copy the file if it is new or has been modified.
+            If Not File.Exists(targetFile) OrElse File.GetLastWriteTime(sourceFile) > File.GetLastWriteTime(targetFile) Then
+                File.Copy(sourceFile, targetFile, True)
+                Debug.Print($"Copied: {sourceFile} -> {targetFile}")
+            Else
+                Debug.Print($"Skipping: {sourceFile} -> {targetFile}")
+            End If
+
+            ' Update the progress bar.
+            ProgressBar1.Value += 1
+            Label3.Invalidate()
+            Label3.Update()
+            Label3.Text = "Synchronizing"
+        Next
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Environment.Exit(0)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SyncPaths()
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
